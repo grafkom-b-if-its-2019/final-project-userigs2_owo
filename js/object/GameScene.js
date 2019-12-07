6f3186dfcd207609e143b9135d945ffa52e660fb
Muhammad Iqbal Imani Atfan <42773954+Iotatfan@users.noreply.github.com>
2019-12-07 21:35:59 +0700

    Initial

---
+++ js/object/GameScene.js
@@ -0,0 +1,126 @@
+var sphereBody, sphereMesh;
+var mass = 5, radius = 1.3;
+var cube
+
+function InitWorld(){
+    //Setup World
+    world = new CANNON.World();
+    world.quatNormalizeSkip = 0;
+    world.quatNormalizeFast = false;
+
+    solver = new CANNON.GSSolver();
+
+    world.defaultContactMaterial.contactEquationStiffness = 1e9;
+    world.defaultContactMaterial.contactEquationRelaxation = 4;
+    
+    solver.iterations = 7;
+    solver.tolerance = 0.1;
+    split = true;
+    if(split)
+        world.solver = new CANNON.SplitSolver(solver);
+    else
+        world.solver = solver;
+
+    world.gravity.set(0,-20,0);
+    world.broadphase = new CANNON.NaiveBroadphase();
+
+    // Create a slippery material (friction coefficient = 0.0)
+    physicsMaterial = new CANNON.Material("slipperyMaterial");
+    physicsContactMaterial = new CANNON.ContactMaterial(physicsMaterial, 
+        physicsMaterial, 0.0, 0.3);
+    
+    // Add material to the world
+    world.addContactMaterial(physicsContactMaterial);
+
+    // Create Sphere
+    sphereShape = new CANNON.Sphere(radius);
+    sphereBody = new CANNON.Body({mass:mass});
+    sphereBody.addShape(sphereShape);
+    sphereBody.position.set(0,10,0);
+    sphereBody.linearDamping = 0.9;
+    world.addBody(sphereBody);
+
+    // Create Plane
+    groundShape = new CANNON.Plane();
+    groundBody = new CANNON.Body({mass: 0});
+    groundBody.addShape(groundShape);
+    groundBody.quaternion.setFromAxisAngle(new CANNON.Vec3(1,0,0),-Math.PI/2);
+    world.addBody(groundBody);
+}
+
+function InitScene(){
+    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+    scene = new THREE.Scene();
+
+    ambient = new THREE.AmbientLight( 0x111111);
+    scene.add(ambient);
+
+    light = new THREE.SpotLight( 0xffffff );
+    light.position.set(100,100,0);
+    light.target.position.set(0,0,0);
+
+    if(true) {
+        light.castShadow = true;
+
+        light.shadowCameraNear = 20;
+        light.shadowCameraFar = 50;
+        light.shadowCameraFov = 40;
+    }
+    scene.add(light);
+
+    controls = new PointerLockControls(camera, sphereBody);
+    scene.add(controls.getObject());
+
+    //Sphere
+    var sphereGeometry = new THREE.SphereGeometry(radius);
+    var sphereMaterial = new THREE.MeshLambertMaterial({color: 0xffff00});
+    sphereMesh = new THREE.Mesh(sphereGeometry, sphereMaterial);
+    sphereMesh.position.copy(sphereBody.position);
+    sphereMesh.castShadow = true;
+    sphereMesh.receiveShadow = true;
+    scene.add(sphereMesh);
+
+    // floor
+    geoPlane = new THREE.PlaneGeometry( 300, 300, 1, 1);
+    geoPlane.applyMatrix(new THREE.Matrix4().makeRotationX( -Math.PI / 2));
+
+    material = new THREE.MeshPhongMaterial({color: 0xdddddd});
+
+    planeMesh = new THREE.Mesh(geoPlane, material);
+    planeMesh.castShadow = true;
+    planeMesh.receiveShadow = true;
+    scene.add(planeMesh);
+
+    cube = new Cube(0,2.5,2)
+    
+    // city = new City();
+
+    // var gun = new THREE.OBJMTLLoader();
+    //     var mat = function (model) {
+    //         gunMesh = model;
+    //         // mesh.loadTexture(texture);
+    //         gunMesh.applyMatrix(new THREE.Matrix4().makeTranslation(0.5,-0.3,-1.4));
+    //         gunMesh.scale.set(0.3, 0.3, 0.3);
+    //         camera.add(gunMesh);
+    //     }
+
+    //     gun.load('./assets/OBJ/SniperRifle.obj', './assets/OBJ/SniperRifle.mtl', mat);
+
+}
+
+function SceneUpdate(){
+    if(controls.enabled){
+        world.step(dt);
+
+        // Update ball positions
+        for(var i=0; i<balls.length; i++){
+            ballMeshes[i].position.copy(balls[i].position);
+            ballMeshes[i].quaternion.copy(balls[i].quaternion);
+        }
+    }
+    UpdateCube();
+    controls.update( Date.now() - time );
+    sphereMesh.position.copy(sphereBody.position);
+    renderer.render( scene, camera );
+    time = Date.now();
+}
---
+++ main.js
@@ -0,0 +1,181 @@
+var controls, time = Date.now();
+var balls = [], ballMeshes = [];
+var dt = 1/60;
+var stat, scene;
+
+function Init(){
+    Start();
+    InitWorld();
+    InitScene();
+
+    stat = InitStats();
+
+    var renderer = new THREE.WebGLRenderer();
+    renderer.shadowMapEnabled = true;
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    
+    document.body.appendChild(renderer.domElement);
+    
+    Update();
+}
+
+function Start(){
+    var blocker = document.getElementById( 'blocker' );
+    var instructions = document.getElementById( 'instructions' );
+
+    var havePointerLock = 'pointerLockElement' in document || 'mozPointerLockElement' in document || 'webkitPointerLockElement' in document;
+
+    if ( havePointerLock ) {
+
+        var element = document.body;
+
+
+        var pointerlockchange = function ( event ) {
+
+            if ( document.pointerLockElement === element || document.mozPointerLockElement === element || document.webkitPointerLockElement === element ) {
+
+                controls.enabled = true;
+
+                blocker.style.display = 'none';
+
+            } else {
+
+                controls.enabled = false;
+
+                blocker.style.display = '-webkit-box';
+                blocker.style.display = '-moz-box';
+                blocker.style.display = 'box';
+
+                instructions.style.display = '';
+
+            }
+
+        }
+
+        var pointerlockerror = function ( event ) {
+            instructions.style.display = '';
+        }
+
+        // Hook pointer lock state change events
+        document.addEventListener( 'pointerlockchange', pointerlockchange, false );
+        document.addEventListener( 'mozpointerlockchange', pointerlockchange, false );
+        document.addEventListener( 'webkitpointerlockchange', pointerlockchange, false );
+
+        document.addEventListener( 'pointerlockerror', pointerlockerror, false );
+        document.addEventListener( 'mozpointerlockerror', pointerlockerror, false );
+        document.addEventListener( 'webkitpointerlockerror', pointerlockerror, false );
+
+        instructions.addEventListener( 'click', function ( event ) {
+            instructions.style.display = 'none';
+
+            // Ask the browser to lock the pointer
+            element.requestPointerLock = element.requestPointerLock || element.mozRequestPointerLock || element.webkitRequestPointerLock;
+
+            if ( /Firefox/i.test( navigator.userAgent ) ) {
+
+                var fullscreenchange = function ( event ) {
+
+                    if ( document.fullscreenElement === element || document.mozFullscreenElement === element || document.mozFullScreenElement === element ) {
+
+                        document.removeEventListener( 'fullscreenchange', fullscreenchange );
+                        document.removeEventListener( 'mozfullscreenchange', fullscreenchange );
+
+                        element.requestPointerLock();
+                    }
+
+                }
+
+                document.addEventListener( 'fullscreenchange', fullscreenchange, false );
+                document.addEventListener( 'mozfullscreenchange', fullscreenchange, false );
+
+                element.requestFullscreen = element.requestFullscreen || element.mozRequestFullscreen || element.mozRequestFullScreen || element.webkitRequestFullscreen;
+
+                element.requestFullscreen();
+
+            } else {
+
+                element.requestPointerLock();
+
+            }
+
+        }, false );
+
+    } else {
+
+        instructions.innerHTML = 'Your browser doesn\'t seem to support Pointer Lock API';
+
+    }
+}
+
+function InitStats() {
+
+    var stats = new Stats();
+    stats.setMode(0); // 0: fps, 1: ms
+
+    // Align top-left
+    stats.domElement.style.position = 'absolute';
+    stats.domElement.style.left = '0px';
+    stats.domElement.style.top = '0px';
+
+    document.getElementById("Stats-output").appendChild(stats.domElement);
+
+    return stats;
+}
+
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+}
+
+function Update() {
+    requestAnimationFrame( Update );
+    
+    SceneUpdate();
+    
+    stat.update();
+
+}
+
+Init();
+
+var ballShape = new CANNON.Sphere(0.1);
+var ballGeometry = new THREE.SphereGeometry(ballShape.radius, 32, 32);
+var shootDirection = new THREE.Vector3();
+var shootVelo = 10;
+var projector = new THREE.Projector();
+function getShootDir(targetVec){
+    var vector = targetVec;
+    targetVec.set(0,0,1);
+    projector.unprojectVector(vector, camera);
+    var ray = new THREE.Ray(sphereBody.position, vector.sub(sphereBody.position).normalize() );
+    targetVec.copy(ray.direction);
+}
+
+window.addEventListener("click",function(e){
+    if(controls.enabled==true){
+        var x = sphereBody.position.x+0.5;
+        var y = sphereBody.position.y-0.3;
+        var z = sphereBody.position.z-1.4;
+        var ballBody = new CANNON.Body({ mass: 1 });
+        ballBody.addShape(ballShape);
+        var ballMesh = new THREE.Mesh( ballGeometry, material );
+        world.addBody(ballBody);
+        scene.add(ballMesh);
+        ballMesh.castShadow = true;
+        ballMesh.receiveShadow = true;
+        balls.push(ballBody);
+        ballMeshes.push(ballMesh);
+        getShootDir(shootDirection);
+        ballBody.velocity.set(  shootDirection.x * shootVelo,
+                                shootDirection.y * shootVelo,
+                                shootDirection.z * shootVelo);
+
+        // Move the ball outside the player sphere
+        x += shootDirection.x * (sphereShape.radius*1.02 + ballShape.radius);
+        y += shootDirection.y * (sphereShape.radius*1.02 + ballShape.radius);
+        z += shootDirection.z * (sphereShape.radius*1.02 + ballShape.radius);
+        ballBody.position.set(x,y,z);
+        ballMesh.position.set(x,y,z);
+    }
+});
