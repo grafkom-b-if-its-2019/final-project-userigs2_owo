31033397f5a9642510d37b06ab5197b1b8874ede
idlavoNuyaB <36811359+idlavoNuyaB@users.noreply.github.com>
2019-12-14 16:36:44 +0700

    tes

--- main.js
+++ main.js
@@ -8,13 +8,8 @@ function Init(){
     Start();
     InitWorld();
     InitScene();
-<<<<<<< HEAD
-    
-    InitEnemy();
-=======
     InitGameObject();
     initEnemy();
->>>>>>> 1186f5ab902afd357678303925665d70cea00243
 
     stat = InitStats();
 
